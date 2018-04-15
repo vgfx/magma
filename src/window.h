014d8ea77f0cdaeeefbba38b291f132b7307b483
vgfx <zalbard@gmail.com>
2018-04-15 21:15:05 +0200

    Fix formatting

--- src/window.h
+++ src/window.h
@@ -8,36 +8,36 @@
 class Window
 {
 public:
-   RULE_OF_FIVE_MOVE_ONLY(Window);
+    RULE_OF_FIVE_MOVE_ONLY(Window);
 
-   // Creates a window; takes the client (drawable) area dimensions (in pixels) as input.
-   Window(const uint16_t width, const uint16_t height);
+    // Creates a window; takes the client (drawable) area dimensions (in pixels) as input.
+    Window(const uint16_t width, const uint16_t height);
 
-   // Makes the window visible.
-   void Show() const;
+    // Makes the window visible.
+    void Show() const;
 
-   // Makes the window invisible.
-   void Hide() const;
+    // Makes the window invisible.
+    void Hide() const;
 
-   // Returns the handle of the application.
-   HINSTANCE Instance() const;
+    // Returns the handle of the application.
+    HINSTANCE Instance() const;
 
-   // Returns the handle of the window.
-   HWND Handle() const;
+    // Returns the handle of the window.
+    HWND Handle() const;
 
-   // Returns the client (drawable) area width (in pixels).
-   uint16_t Width() const;
+    // Returns the client (drawable) area width (in pixels).
+    uint16_t Width() const;
 
-   // Returns the client (drawable) area height (in pixels).
-   uint16_t Height() const;
+    // Returns the client (drawable) area height (in pixels).
+    uint16_t Height() const;
 
-   // Displays information (in milliseconds) in the title bar.
-   // 'cpuFrameTime', 'gpuFrameTime': the frame times (in milliseconds) of CPU/GPU time lines. 
-   void UpdateTitle(const float cpuFrameTime, const float gpuFrameTime) const;
+    // Displays information (in milliseconds) in the title bar.
+    // 'cpuFrameTime', 'gpuFrameTime': the frame times (in milliseconds) of CPU/GPU time lines.
+    void UpdateTitle(const float cpuFrameTime, const float gpuFrameTime) const;
 
 private:
 
-   uint16_t  m_width, m_height; // Client area dimensions
-   HWND      m_hwnd;
-   HINSTANCE m_hinst;
+    uint16_t  m_width, m_height; // Client area dimensions
+    HWND      m_hwnd;
+    HINSTANCE m_hinst;
 };
